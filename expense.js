a12b085bd5f6ee0996226860c410c771abf38c80
Ritik800024 <125725939+Ritik800024@users.noreply.github.com>
2023-07-02 13:43:00 +0530

    tokens

--- expense.js
+++ expense.js
@@ -1,13 +1,40 @@
-const express=require('express')
+const Expense=require('../models/expense')
+const jwt=require('jsonwebtoken')
 
-const expensecontroller=require('../controllers/expense')
+exports.addexpense=('/addexpense',async (req,res)=>{
+    const expense=req.body.expense
+    const description=req.body.desc
+    const category=req.body.category
+    const date_of_expense=req.body.date_of_expense
 
-const router=express.Router()
+    const data=await Expense.create({
+        expense,
+        description,
+        category,
+        date_of_expense
+    })
+    res.status(200).json({newexpense:data})
+})
 
-router.post('/addexpense',expensecontroller.addexpense)
+exports.deleteexpense=('/deleteexpense',async (req,res)=>{
+    const token=req.headers['authorization']
+    console.log(token)
+    const decode=jwt.verify(token,'kjbdbdqwidqd654635465dq4q4d6qw4d65q4wd6qw4d64qw68d4q54d65q4d31qww65d46qww4d45d4123as5d8w2224544xdcfs')
+    await Expense.destroy({
+        where:{
+            exUserId:decode.userid
+        }
+    })
+})
 
-router.get('/getallexpense',expensecontroller.getallexpense)
-
-router.delete('/deleteexpense/:expenseid',expensecontroller.deleteexpense)
-
-module.exports=router;
+exports.getallexpense=('/getallexpense',async (req,res)=>{
+    const token=req.headers['authorization']
+    console.log(token)
+    const decode=jwt.verify(token,'kjbdbdqwidqd654635465dq4q4d6qw4d65q4wd6qw4d64qw68d4q54d65q4d31qww65d46qww4d45d4123as5d8w2224544xdcfs')
+    const data=await Expense.findAll({
+        where:{
+            exUserId:decode.userid
+        }
+    })
+    res.status(200).json({allexpense:data})
+})
--- user.js
+++ user.js
@@ -1,5 +1,6 @@
 const User=require('../models/user')
 const bcrypt=require('bcrypt')
+const jwt=require('jsonwebtoken')
 
 exports.adduser=('/adduser',async (req,res)=>{
     const name =req.body.name;
@@ -45,7 +46,7 @@ exports.finduser=('/finduser',async (req,res)=>{
             })
             bcrypt.compare(pass,data[0].pass,(err,result)=>{
                 if(!err){
-                    res.status(200).json({success:true, message:"User loged in successfully"})
+                    res.status(200).json({success:true, message:"User loged in successfully", token:generateAccessToken(data[0].id)})
                 }
                 else{
                     res.status(401).json({success:false,message:"Incorrect password"})
@@ -80,4 +81,8 @@ async function userexist(email){
     else{
         return false
     }
+}
+
+function generateAccessToken(id){
+    return jwt.sign({userid:id},'kjbdbdqwidqd654635465dq4q4d6qw4d65q4wd6qw4d64qw68d4q54d65q4d31qww65d46qww4d45d4123as5d8w2224544xdcfs')
 }
